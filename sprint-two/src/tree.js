6ef66101c609f0c1e5879ab5713b000985c8cfad
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-13 00:16:24 -0700

    one case is not working

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -55,4 +55,7 @@ console.log(example.contains(5))
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-/
+// .children property, an array containing a number of subtrees
+// .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+// A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+// What is the time complexity of the above functions?
