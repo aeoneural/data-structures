49cb1518b64a743b1030011fd57fdd572e5e73d0
aikerim <aygerim.sauletkhan@gmail.com>
2017-10-12 23:49:09 -0700

    completed functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,18 +2,66 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+
+  var key = 0;
+  var start = 0;
   var storage = {};
+  var key = 0;
+  var end = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+<<<<<<< HEAD
+    storage[key] = value; 
+    console.log(storage)
+=======
+    storage[key] = value;
+>>>>>>> d2d0439071ed8117a56d4603eae85b948150c582
+    key++;
   };
+  // console.log(storage) why doesn't print ? 
 
   someInstance.dequeue = function() {
+<<<<<<< HEAD
+    var firstElement = storage[end]
+    if(storage[end]){    
+        delete storage[end];
+        end++;
+    }
+    return firstElement
   };
 
   someInstance.size = function() {
+    return key-end;
+=======
+    
+    if (storage[start]) { 
+      var last = storage[start];
+      delete last;
+      start++;
+    }
+
+    return last;
+    
+  };
+
+  someInstance.size = function() {
+
+    return key-start;
+>>>>>>> d2d0439071ed8117a56d4603eae85b948150c582
   };
 
   return someInstance;
+
+  //test
 };
+
+
+<<<<<<< HEAD
+=======
+
+
+
+
+>>>>>>> d2d0439071ed8117a56d4603eae85b948150c582
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,55 @@
 var Stack = function() {
   var someInstance = {};
+  var key = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+<<<<<<< HEAD
+    key++; 
+    storage[key] = value;
+    console.log(storage)
+ 
   };
 
   someInstance.pop = function() {
+    var lastElement = storage[key]
+    if(key){ 
+      delete storage[key];
+      key--;
+    }
+    return lastElement
+=======
+    key++;
+    storage[key] = value;
+      
+  };
+
+  someInstance.pop = function() {
+    var last = storage[key];
+    if (storage[key]) {
+      
+      delete storage[key];
+      key--;
+    }
+    return last;
+>>>>>>> d2d0439071ed8117a56d4603eae85b948150c582
   };
 
   someInstance.size = function() {
+    return key;
+<<<<<<< HEAD
+
+=======
+>>>>>>> d2d0439071ed8117a56d4603eae85b948150c582
   };
 
   return someInstance;
 };
+
+var a = Stack()
+a.push(1)
+
